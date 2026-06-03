4415b63efda42ff059fc075550a96d020dd6f798
L.L. <40061145+LL264a@users.noreply.github.com>
2026-06-03 12:58:22 +0800

    test: add internal/task/store_bench_test.go

---
+++ internal/task/store_bench_test.go
@@ -0,0 +1,48 @@
+﻿package task
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func BenchmarkTaskStoreSave(b *testing.B) {
+	dir := b.TempDir()
+	store := NewTaskStore(dir)
+	tasks := make([]*Task, b.N)
+	for i := range tasks {
+		tasks[i] = NewTask("https://example.com/file.bin", "/tmp/file.bin")
+		tasks[i].Priority = i % 10
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = store.Save(tasks[i])
+	}
+}
+
+func BenchmarkTaskStoreLoad(b *testing.B) {
+	dir := b.TempDir()
+	store := NewTaskStore(dir)
+	t1 := NewTask("https://example.com/file.bin", "/tmp/file.bin")
+	_ = store.Save(t1)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = store.Load(t1.ID)
+	}
+}
+
+func BenchmarkTaskWriteAndRead(b *testing.B) {
+	dir := b.TempDir()
+	data := make([]byte, 1<<20)
+	for i := range data {
+		data[i] = byte(i % 251)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p := filepath.Join(dir, "x.bin")
+		if err := os.WriteFile(p, data, 0o644); err != nil {
+			b.Fatal(err)
+		}
+		_, _ = os.ReadFile(p)
+	}
+}
